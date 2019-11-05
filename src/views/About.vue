694cd83564c9b49d8a6030dee57135bd20714fda
repolloja_sd2023@CEACCS.NET <repolloja_sd2023@CEACCS.NET>
2019-11-05 16:45:59 +0800

    Checked by sir

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,15 +2,20 @@
   <div class="about">
     <h1>AUTHORS</h1>
     <b-container fluid>
-      <b-card v-for="author in authors" v-bind:key="author">
-      <b-card bg-variant="info" text-variant="white" header="Profile" class="text-center">
-          <Member v-bind:picture="author.imag" v-bind:name=author.name></Member>
-          <b-card-text>{{author.text}}</b-card-text>
-      </b-card>            
-    </b-card> 
+      <b-row>
+        <b-card deck v-for="author in authors" v-bind:key="author" style="width: 25%">
+          <b-col>
+            <b-card bg-variant="info" text-variant="white" header="Profile" class="text-center">
+              <Member v-bind:picture="author.imag" v-bind:name="author.name"></Member>
+              <b-card-text>{{author.text}}</b-card-text>
+            </b-card>
+          </b-col>
+        </b-card>
+      </b-row>
     </b-container>
   </div>
 </template>
+
 <script>
 import Member from "@/components/members.vue";
 
@@ -18,10 +23,26 @@ export default {
   data() {
     return {
       authors: [
-        { name: "Sharmen Claire Avila", imag: require("@/assets/jane.jpg"), text: "Full Stack Developer"},
-        { name: "Jane Repollo", imag: require("@/assets/sharmen.jpg") , text: "Web Devloper"},
-        { name: "Jonathan Rivas", imag: require("@/assets/jonathan.jpg"), text: "Full Stack Developer"},
-        { name: "Redgie Gravador", imag: require("@/assets/red.png"), text: "Full Stack Developer"}
+        {
+          name: "Jonathan Rivas",
+          imag: require("@/assets/jonathan.jpg"),
+          text: "Full Stack Developer"
+        },
+        {
+          name: "Redgie Gravador",
+          imag: require("@/assets/red.png"),
+          text: "Full Stack Developer"
+        },
+        {
+          name: "Sharmen Claire Avila",
+          imag: require("@/assets/sharmen.jpg"),
+          text: "Full Stack Developer"
+        },
+        {
+          name: "Jane Repollo",
+          imag: require("@/assets/jane.jpg"),
+          text: "Web Devloper"
+        }
       ]
     };
   },
